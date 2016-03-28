2d08897f0e60d3179f2fa3cc83c45900a6a6d7b3
Simon Andrews <simon.andrews212@gmail.com>
2016-03-28 17:03:31 -0400

    Move javascript into its own file

---
+++ js/spinner.js
@@ -0,0 +1,41 @@
+---
+---
+
+var subjects = [
+	{% for page in site.pages %}
+		{% if page.title %}
+			{
+				title:"{{ page.title }}",
+				permalink:"{{ page.permalink }}",
+			},
+		{% endif %}
+	{% endfor %}
+];
+
+var lastSubject = null;
+function getRandomSubject() {
+	var candidate = subjects[Math.floor(Math.random() * subjects.length)];
+	return candidate;
+	/*
+	while(true) {
+		if(JSON.stringify(candidate) != JSON.stringify(lastSubject)) {
+			lastSubject = candidate;
+			return candidate;
+		}
+	}
+	*/
+}
+
+function clickCallback() {
+	var subjectNode = document.getElementById("subject");
+	var subjectObj = getRandomSubject();
+	subjectNode.innerHTML = subjectObj.title.toLowerCase();
+	var qrCodeNode = document.getElementById("qrcode");
+	while(qrCodeNode.firstChild) {
+		qrCodeNode.removeChild(qrCodeNode.firstChild);
+	}
+	$('#qrcode').qrcode("{{ site.url }}" + subjectObj.permalink);
+}
+
+document.addEventListener("click", clickCallback);
+document.body.click();
